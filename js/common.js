ec74e6fba3b2ae435b5ee263748b3dac3c4c6e5d
handarmar <44291181+handarmar@users.noreply.github.com>
2018-10-29 10:51:01 +0500

    new-29.10

---
+++ js/common.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+	$(".ajax-form").submit(function() {
+		$.ajax({
+			type: "POST",
+			url: "/mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			window.location.replace('http://skymedia.tech/good');
+			$("#form").trigger("reset");
+		});
+		return false;
+	});
+	
+});
--- php/mail.php
+++ php/mail.php
@@ -1,42 +1,18 @@
 <?php
-$method = $_SERVER['REQUEST_METHOD'];
-//Script Foreach
-$c = true;
-if ( $method === 'POST' ) {
-	$project_name = trim($_POST["project_name"]);
-	$admin_email  = trim($_POST["admin_email"]);
-	$form_subject = trim($_POST["form_subject"]);
-	foreach ( $_POST as $key => $value ) {
-		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
-			$message .= "
-			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
-				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
-				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
-			</tr>
-			";
-		}
-	}
-} else if ( $method === 'GET' ) {
-	$project_name = trim($_GET["project_name"]);
-	$admin_email  = trim($_GET["admin_email"]);
-	$form_subject = trim($_GET["form_subject"]);
-	foreach ( $_GET as $key => $value ) {
-		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
-			$message .= "
-			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
-				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
-				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
-			</tr>
-			";
-		}
-	}
-}
-$message = "<table style='width: 100%;'>$message</table>";
-function adopt($text) {
-	return '=?UTF-8?B?'.Base64_encode($text).'?=';
-}
-$headers = "MIME-Version: 1.0" . PHP_EOL .
-"Content-Type: text/html; charset=utf-8" . PHP_EOL .
-'From: '.adopt($project_name).' <'.$admin_email.'>' . PHP_EOL .
-'Reply-To: '.$admin_email.'' . PHP_EOL;
-mail($admin_email, adopt($form_subject), $message, $headers );
+
+$recepient = "skymedia+lead@mail.amocrm.ru"; 
+$sitename = "Skymedia.tech"; 
+
+$name = trim($_POST["name"]);
+$surname = trim($_POST["surname"]); 
+$phone = trim($_POST["phone"]); 
+$text = trim($_POST["text"]); 
+$mail = trim($_POST["mail"]); 
+$message = "Имя: $name $surname \nТелефон: $phone \nПочта: $mail"; 
+$message2 = "Спасибо за Вашу заявку, в ближайшее время с Вами свяжется наш менеджер, с ув. команда SkyMedia"; 
+
+$pagetitle = "Новая заявка с сайта \"$sitename\" от \"$name $surname\""; 
+$pagetitle2 = "Ваша заявка с сайта \"$sitename\" принята"; 
+
+mail($recepient, $pagetitle, $message, "From: noreply@skymedia.tech\nContent-type: text/plain; charset=\"utf-8\"\n From: $recepient"); 
+mail($mail, $pagetitle2, $message2, "From: noreply@skymedia.tech\nContent-type: text/plain; charset=\"utf-8\"\n From: $mail");
